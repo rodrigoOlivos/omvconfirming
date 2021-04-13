4a4b91b5c700e8b727ac6210ed7657198bc83a80
rodrigo.patricio.olivos.pavez@gmail.com <rodrigo.patricio.olivos.pavez@gmail.com>
2021-04-13 18:09:25 -0400

    Inicial del Footer / Header / Nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
