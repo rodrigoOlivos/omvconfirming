cf176e601f48cfe74b07c5c9ce0fd75cab018c31
Orlando <orlasep@gmail.com>
2021-04-12 22:19:08 -0400

    validación vista login v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  reactiveForm:FormGroup;
+  submitted: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.reactiveForm = this.formBuilder.group({
+      rutempresa: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      rutpersona: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      clave: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    })
+  }
+
+  // form = new FormGroup({
+  // })
+  //
+  get f() {
+    return this.reactiveForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.reactiveForm.invalid) {
+      return;
+    }
+    // console.log(this.form.value)
+  }
 
   ngOnInit(): void {
   }
