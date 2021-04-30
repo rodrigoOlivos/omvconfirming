31d765a7b7b790626ac7f6f1358dd47e34673348
rodrigo.patricio.olivos.pavez@gmail.com <rodrigo.patricio.olivos.pavez@gmail.com>
2021-04-30 03:14:00 -0400

    integracion de paginas de administracion

--- src/app/components/app-tasas/ngx-datatable-tasas/ngx-datatable-tasas.component.ts
+++ src/app/components/app-tasas/ngx-datatable-tasas/ngx-datatable-tasas.component.ts
@@ -16,10 +16,15 @@ export class NgxDatatableTasasComponent {
   idTipoTabla = 12;
   encabezadoTabla: any[] = [];
   columnaReferencia: any[] = [];
+  preCargaRows: any[] = [];
+  cargaRows: any[] = [];
+  itemsRows = {
+  };
   @Input()
   idTipoMoneda = 1;
   @Input()
   tasaEdit = false;
+
 
   ColumnMode = ColumnMode;
   columns: any;
@@ -38,18 +43,58 @@ export class NgxDatatableTasasComponent {
     );
     ngxDataF32Service.getDataf32(this.idTipoTabla, 0, 0, this.idTipoMoneda).subscribe(data => {
         console.log('datatable');
-        console.log(data.arrayOfRow32.row32);
+        this.preCargaRows = data.arrayOfRow32.row32;
+        let columnaref  = 0 ;
+        let columncount  = 0;
+        let rowcount = 2;
+        let ciclo = 0;
+        this.preCargaRows.forEach((value) => {
+
+          if (columncount === 0){
+            columncount = value.idRangoMonto;
+            rowcount = 2;
+            // @ts-ignore
+            this.itemsRows[1] = '>' + this.columnaReferencia[columnaref].montoDesde
+              + ', <= ' + this.columnaReferencia[columnaref].montoHasta;
+            console.log(this.columnaReferencia);
+            columnaref++;
+          }
+          if (columncount === value.idRangoMonto)
+          {
+            // @ts-ignore
+            this.itemsRows[rowcount] = value.tasa;
+            rowcount++;
+            ciclo++;
+            if (ciclo === this.preCargaRows.length){
+              this.cargaRows.push(this.itemsRows);
+            }
+          }else{
+            columncount = value.idRangoMonto;
+            rowcount = 2;
+            this.cargaRows.push(this.itemsRows);
+            this.itemsRows = {};
+            // @ts-ignore
+            this.itemsRows[1] = '>' + this.columnaReferencia[columnaref].montoDesde + ', <= '
+              + this.columnaReferencia[columnaref].montoHasta;
+            columnaref++;
+            // @ts-ignore
+            this.itemsRows[rowcount] = value.tasa;
+            rowcount++;
+            ciclo++;
+          }
+        });
+        this.rows = this.cargaRows;
       },
       err => {
         console.log(err);
       }
     );
 
-    this.rows = [{1: '>0, <=3.000.000', 2: '0.4', 3: '0.5', 4: '0.7'},
+    /*his.rows = [{1: '>0, <=3.000.000', 2: '0.4', 3: '0.5', 4: '0.7'},
       {1: '>123, <=3.000', 2: '0.4', 3: '0.6', 4: '0.7'},
       {1: '>3.000.000, <=30.000.000', 2: '0.4', 3: '0.6', 4: '0.7'},
       {1: '>30.000.000, <=300.000.000', 2: '0.4', 3: '0.6', 4: '0.7'}
-    ];
+    ];*/
     this.columns = [{name: '$/dias'}, {name: '>60,<=180'}, {name: '>60,<=180'}, {name: '>60,<=180'}];
   }
 
