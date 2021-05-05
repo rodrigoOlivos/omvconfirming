46038e73daac3605d7dd49274692e2047db21334
rodrigo.patricio.olivos.pavez@gmail.com <rodrigo.patricio.olivos.pavez@gmail.com>
2021-05-05 00:45:49 -0400

    add agf-provider.service.ts

---
+++ src/app/services/agf-provider.service.ts
@@ -0,0 +1,86 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
+
+const API_AGF = 'http://52.60.210.85:8080/omvagf';
+
+export class AgfProviderService {
+
+  constructor(private http: HttpClient,
+              private cookieService: CookieService) {
+  }
+
+  getDataf29(idTipoTabla: number, idTipoMoneda: number): Observable<any> {
+    const cookieToken = this.cookieService.get('token_access');
+    return this.http.get(API_AGF + '/api/f29/' + idTipoTabla + '/' + idTipoMoneda, {
+      headers: {
+        'Set-Header-Api': cookieToken
+      }, observe: 'body'
+    });
+  }
+  getDataf30( idTipoMat: number, idRoll: number, cantRangoMontos: number, cantRangoPlazos: number, idTipoMoneda: number,
+    arrayRangoPlazos: any[], arrayRangoMontos: any[] ): Observable<any> {
+    const bodyF30 = {
+      idTipoMatriz: idTipoMat,
+      idRol: idRoll,
+      cantidadRangosMontos: cantRangoMontos,
+      cantidadRangosPlazos: cantRangoPlazos,
+      moneda: idTipoMoneda,
+      arrayOfRow301: {
+        row301: arrayRangoPlazos
+      },
+      arrayOfRow302: {
+        row302: arrayRangoMontos
+      }
+    };
+    const cookieToken = this.cookieService.get('token_access');
+    return this.http.post(API_AGF +'/api/f30/', bodyF30, {
+      headers: {
+        'Set-Header-Api': cookieToken
+      }, observe: 'body'
+    });
+  }
+  getDataf31(idTipoTabla: number, idTipoMoneda: number ): Observable<any> {
+    const cookieToken = this.cookieService.get('token_access');
+    return this.http.get(API_AGF+'/api/f31/' + idTipoTabla + '/' + idTipoMoneda, {
+      headers: {
+        'Set-Header-Api': cookieToken
+      }, observe: 'body'
+    });
+  }
+  getDataf32(idTipoTabla: number, idComprador: number, idProveedor: number, idTipoMoneda: number): Observable<any> {
+    const cookieToken = this.cookieService.get('token_access');
+    return this.http.get(API_AGF + '/api/f32/' + idTipoTabla + '/' + idComprador + '/' + idProveedor + '/' + idTipoMoneda, {
+      headers: {
+        'Set-Header-Api': cookieToken
+      }, observe: 'body'
+    });
+  }
+  getDataf33(
+    idTipoMat: number,
+    idTipoMoneda: number,
+    idComprador: number,
+    idProveedor: number,
+    arrayCostoFondo: any[]
+  ): Observable<any> {
+    const cookieToken = this.cookieService.get('token_access');
+    const bodyF33 = {
+      idTipoMatriz: idTipoMat,
+      moneda: idTipoMoneda,
+      idCmp: idComprador,
+      idPrv: idProveedor,
+      arrayOfRow33: {
+        row33: arrayCostoFondo
+      }
+    };
+    return this.http.post(API_AGF + '/api/f33/', bodyF33, {
+      headers: {
+        'Set-Header-Api': cookieToken
+      }, observe: 'body'
+    });
+  }
+
+
+
+}
